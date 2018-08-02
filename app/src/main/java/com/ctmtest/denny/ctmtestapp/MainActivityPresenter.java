ba612e3ea84cf150878dfd55c6c711c6a496e0e0
DennyWhiteHouse <casabiancadenny@gmail.com>
2018-08-02 11:24:11 +0200

    feat: Added a MainActivityPresenter to start implementing a bit of architecture
    As early stage the Presenter builder is still overwhelmed by depedencies (building of Fetching Obeject and receving the Obeject)

--- app/src/main/java/com/ctmtest/denny/ctmtestapp/MainActivityPresenter.java
+++ app/src/main/java/com/ctmtest/denny/ctmtestapp/MainActivityPresenter.java
@@ -1,7 +1,9 @@
 package com.ctmtest.denny.ctmtestapp;
 
+import android.content.Context;
 import android.view.View;
 
+import com.ctmtest.denny.ctmtestapp.data.FetchImages;
 import com.ctmtest.denny.ctmtestapp.data.ImageListObject;
 
 import java.util.ArrayList;
@@ -9,8 +11,11 @@ import java.util.ArrayList;
 public class MainActivityPresenter {
     private ArrayList<ImageListObject> imageArrayList;
     private View view;
+    private FetchImages fetchImages;
 
     public MainActivityPresenter(View view){
         this.view=view;
+        fetchImages = new FetchImages(view.getContext());
+        imageArrayList = fetchImages.getImageJSON();
     }
 }
